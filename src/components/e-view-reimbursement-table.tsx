985cc6903ad01f222a227b5a92bc876cc5890f84
Daniel J Belenski <38082687+Belesnkid@users.noreply.github.com>
2022-01-28 16:15:46 -0500

    Update e-view-reimbursement-table.tsx

--- src/components/e-view-reimbursement-table.tsx
+++ src/components/e-view-reimbursement-table.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ReimbursementRequest from "../dtos/reimbursement-request";
 import EViewReimbursementRow from "./e-view-reimbursement-row";
 
@@ -7,15 +7,20 @@ export default function EViewReimbursementTable(props:{empID:string}){
     
     const [list,setList] = useState([]);
 
-    async function getReimbursements(){
-        const response = await axios.get(`http://localhost:3001/reimbursements/employee/${props.empID}`);
-        const reimbursements:ReimbursementRequest[] = await response.data;
-        setList(reimbursements);
-    }
+    // async function getReimbursements(){
+    //     const response = await axios.get(`http://localhost:3001/reimbursements/employee/${props.empID}`);
+    //     const reimbursements:ReimbursementRequest[] = await response.data;
+    //     setList(reimbursements);
+    // }
 
     useEffect(() => {
-        getReimbursements();
-    }, [list])
+        (async()=>{
+            const response = await axios.get(`http://localhost:3001/reimbursements/employee/${props.empID}`);
+            const reimbursements:ReimbursementRequest[] = await response.data;
+            setList(reimbursements);
+        })()
+     
+    }, [])
 
     const tableRows = list.map(r => <EViewReimbursementRow key={r.id} {...r}/>);
 
@@ -36,6 +41,6 @@ export default function EViewReimbursementTable(props:{empID:string}){
                 {tableRows.length > 0? tableRows :<p>No Requests to Display</p>}
             </tbody>
         </table>
-        <button onClick={getReimbursements}>Refresh List</button>
+        {/* <button onClick={getReimbursements}>Refresh List</button> */}
     </>);
 }
