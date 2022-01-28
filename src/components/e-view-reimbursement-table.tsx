97ecae702ced43550f165eca0f79384455885948
Daniel J Belenski <38082687+Belesnkid@users.noreply.github.com>
2022-01-28 15:40:32 -0500

    Update e-view-reimbursement-table.tsx

--- src/components/e-view-reimbursement-table.tsx
+++ src/components/e-view-reimbursement-table.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import ReimbursementRequest from "../dtos/reimbursement-request";
 import EViewReimbursementRow from "./e-view-reimbursement-row";
 
@@ -7,15 +7,17 @@ export default function EViewReimbursementTable(props:{empID:string}){
     
     const [list,setList] = useState([]);
 
-    async function getReimbursements(){
-        const response = await axios.get(`http://localhost:3001/reimbursements/employee/${props.empID}`);
-        const reimbursements:ReimbursementRequest[] = await response.data;
+    const getReimbursements = useCallback(() =>{
+        let reimbursements:ReimbursementRequest[] = [];
+        axios.get(`http://localhost:3001/reimbursements/employee/${props.empID}`).then(response => {reimbursements = response.data;});
         setList(reimbursements);
+    },[]);{
+        
     }
 
     useEffect(() => {
         getReimbursements();
-    }, [getReimbursements])
+    }, [useCallback(getReimbursements,[])])
 
     const tableRows = list.map(r => <EViewReimbursementRow key={r.id} {...r}/>);
 
